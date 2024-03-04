1a68a4eff0ec4531efb98b2b2d2987fdfddfe636
jpdal <jpdal98@gmail.com>
2024-03-04 09:37:56 -0300

    feat: criando pool de conexões do banco de dados

--- JDBC/Bytebank/src/main/java/br/com/alura/bytebank/ConnectionFactory.java
+++ JDBC/Bytebank/src/main/java/br/com/alura/bytebank/ConnectionFactory.java
@@ -1,5 +1,8 @@
 package br.com.alura.bytebank;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -9,12 +12,19 @@ public class ConnectionFactory {
     public Connection recuperarConexao (){
 
         try {
-            return DriverManager.getConnection(
-                    "jdbc:postgresql://localhost:5432/byte_bank",
-                    "postgres",
-                    "postgres");
+            return createDataSource().getConnection();
         }catch (SQLException e){
             throw new RuntimeException(e.getMessage());
         }
+    }
+
+    private HikariDataSource createDataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl("jdbc:postgresql://localhost:5432/byte_bank");
+        config.setUsername("postgres");
+        config.setPassword("postgres");
+        config.setMaximumPoolSize(10);
+
+        return new HikariDataSource(config);
     }
 }
